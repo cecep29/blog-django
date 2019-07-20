a175b36963cbbe6119d1de7d6ace134d488c6414
cecep29 <cecepefendi.c2p@gmail.com>
2019-07-20 15:16:29 +0700

    fitur klik halaman

--- artikel/admin.py
+++ artikel/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Artikel
+
+
 # Register your models here.
+class ArtikelAdmin(admin.ModelAdmin):
+    readonly_fields = [
+        'slug',
+        'published',
+        'updated',
+    ]
+
+admin.site.register(Artikel, ArtikelAdmin)
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(), name='home'),
+    url(r'^artikel/', include('artikel.urls', namespace='artikel')),
+    url(r'^$', TemplateView.as_view(template_name = 'index.html'), name='home'),
     url(r'^admin/', admin.site.urls),
 ]
