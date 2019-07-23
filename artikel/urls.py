950e76c876e97ab34a258c7a7857ddb8ff4525fe
cecep29 <cecepefendi.c2p@gmail.com>
2019-07-23 13:41:01 +0700

    artikel per kategori

--- artikel/urls.py
+++ artikel/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import url
 from .views import ( 
     ArtikelListView, 
     ArtikelDetailView,
+    ArtikelKategoriListView,
     )
 
 
 urlpatterns = [
+    url(r'^kategori/(?P<kategori>[\w]+)/(?P<page>\d+)$', ArtikelKategoriListView.as_view(), name='category'),
     url(r'^detail/(?P<slug>[\w-]+)$', ArtikelDetailView.as_view(), name='detail'),
     url(r'^(?P<page>\d+)$', ArtikelListView.as_view(), name='list'),
 ]
--- artikel/views.py
+++ artikel/views.py
@@ -12,8 +12,41 @@ class ArtikelListView(ListView):
     ordering = ['-published']
     paginate_by = 3
 
+    def get_context_data(self, *args, **kwargs):
+        list_kategori = self.model.objects.values_list('kategory', flat=True).distinct()
+        self.kwargs.update({'list_kategori': list_kategori})
+        kwargs = self.kwargs
+        return super().get_context_data(*args, **kwargs)
+
+
+class ArtikelKategoriListView(ListView):
+    model = Artikel
+    template_name = 'artikel/artikel_kategori_list.html'
+    context_object_name = 'artikel_list'
+    ordering = ['-published']
+    paginate_by = 2
+
+    def get_queryset(self):
+        self.queryset = self.model.objects.filter(kategory=self.kwargs['kategori'])
+        return super().get_queryset()
+
+    def get_context_data(self, *args, **kwargs):
+        list_kategori = self.model.objects.values_list('kategory', flat=True).distinct().exclude(kategory=self.kwargs['kategori'])
+        self.kwargs.update({'list_kategori': list_kategori})
+        kwargs = self.kwargs
+        return super().get_context_data(*args, **kwargs)
+
+
 
 class ArtikelDetailView(DetailView):
     model = Artikel
     template_name = 'artikel/artikel_detail.html'
     context_object_name = 'artikel'
+
+    def get_context_data(self, *args, **kwargs):
+        list_kategori = self.model.objects.values_list('kategory', flat=True).distinct()
+        self.kwargs.update({'list_kategori': list_kategori})
+        artikel_serupa = self.model.objects.filter(kategory=self.object.kategory).exclude(id=self.object.id)
+        self.kwargs.update({'artikel_serupa': artikel_serupa})
+        kwargs = self.kwargs
+        return super().get_context_data(*args, **kwargs)
